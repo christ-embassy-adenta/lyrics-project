27202f8a38ad2e8288969ebf4ffaabd84e60a7ce
chrispydev <christianowusu44@gmail.com>
2022-08-05 11:22:20 +0000

    added requirement.txt

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 import lyricsgenius
-import spotipy
 from decouple import config
-
-sp = spotipy.Spotify(auth_manager=spotipy.SpotifyClientCredentials(
-    client_id=config('CLIENT_ID'), client_secret=config('CLIENT_SECRET')))
 
 genius = lyricsgenius.Genius(config('GENIUS_ACCESS_TOKEN'))
